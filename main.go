b2c6c69852092048aa8560508e85b439ee0f309e
manattan <mogimogi0529@eis.hokudai.ac.jp>
2020-10-10 14:29:00 +0900

    関数の戻り値

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import "fmt"
 
-func add(x int, y int) int {
-	return x + y
+func swap(x, y string) (string, string) {
+	return y, x
 }
 
 func main() {
-	fmt.Println(add(42, 15))
+	a, b := swap("Hello", "World")
+	fmt.Println(a, b)
 }
