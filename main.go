beb1ec47db9b4caf5d387a49d45b88e7905b7a58
manattan <mogimogi0529@eis.hokudai.ac.jp>
2020-10-10 14:31:31 +0900

    named return

--- main.go
+++ main.go
@@ -2,11 +2,17 @@ package main
 
 import "fmt"
 
-func swap(x, y string) (string, string) {
-	return y, x
+// func swap(x, y string) (string, string) {
+// 	return y, x
+// }
+
+func split(sum int) (a, b int) {
+	a = sum * 4 / 9
+	b = sum - a
+	return
 }
 
 func main() {
-	a, b := swap("Hello", "World")
-	fmt.Println(a, b)
+	// a, b := swap("Hello", "World")
+	fmt.Println(split(18))
 }
