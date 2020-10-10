98085c67354cd7d5cabc77079821ca7520600fa9
manattan <mogimogi0529@eis.hokudai.ac.jp>
2020-10-10 15:26:33 +0900

    代入

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 // func swap(x, y string) (string, string) {
@@ -17,13 +16,18 @@ import (
 
 // var c, Python, Java bool
 
+var i int = 5
+
 func main() {
 	// a, b := swap("Hello", "World")
 	// var i int
 	// fmt.Println(i, c, Python, Java)
-	x := 3
-	y := 4
-	var f float64 = math.Sqrt(float64(x*x + y*y))
-	z := uint(f)
-	fmt.Println(x, y, f, z)
+
+	// x := 3
+	// y := 4
+	// var f float64 = math.Sqrt(float64(x*x + y*y))
+	// z := uint(f)
+	// fmt.Println(x, y, f, z)
+	i := 10
+	fmt.Printf("iの代入後の型は %T です\n", i)
 }
